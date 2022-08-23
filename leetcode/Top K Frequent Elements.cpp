c741fab77a1d19a1fca4e06e38e361b9689816b8
Zachary Seligman <58638589+Seligmann@users.noreply.github.com>
2022-08-23 12:21:20 -0500

    Update Top K Frequent Elements.cpp

--- leetcode/Top K Frequent Elements.cpp
+++ leetcode/Top K Frequent Elements.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        // using vectors to keep track of each values freq doesn't work because you can have negative values
-        map<int, int> freq;
-        map<int, int> freqIdx;
-        priority_queue<pair<int,int>> sortedFreq;
+        // go thru nums, count occurs of each unique num, using a map to keep track of each unique num and its occurance
+        // using a priority queue, push the num of occurs
+        // iterate through the map of nums and their occurs, and create a multimap with (second, first)
+        // until ret vector is size k, index the multimap with the priority que top, then remove that index in the multimap
+        map<int, int> occurs;
         vector<int> ret;
+        priority_queue<int> sortedOccurs;
+        multimap<int, int> idxOccurs;
         
-        for (int i = 0; i < nums.size(); i++)
-            freq[nums[i]]++;
+        for (int i = 0; i < nums.size(); i++) {
+            occurs[nums[i]]++;
+        }
         
-        for (auto it = freq.begin(); it != freq.end(); it++)
-            freqIdx[it->second] = it->first;
+        for (auto it = occurs.begin(); it != occurs.end(); it++) {
+            idxOccurs.insert(pair<int,int>(it->second, it->first));
+        }
         
-        for (auto it = freq.begin(); it != freq.end(); it++)
-            sortedFreq.push(make_pair(it->second, it->first));
+        for (auto it = occurs.begin(); it != occurs.end(); it++) {
+            sortedOccurs.push(it->second);
+        }
         
-        for (int i = 0; i < k; i++) {
-            ret.push_back(sortedFreq.top().second);
-            sortedFreq.pop();
+        while (ret.size() != k) {
+            // int currTop = sortedOccurs.top();
+            ret.push_back(idxOccurs.find(sortedOccurs.top())->second);
+            idxOccurs.erase(idxOccurs.find(sortedOccurs.top()));
+            sortedOccurs.pop();
         }
         
         return ret;
